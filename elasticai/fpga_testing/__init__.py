0b49cc074fca33afc063ebe77eea5b762c479cba
Andreas Erbslöh <andreas.erbsloeh@uni-due.de>
2026-03-02 18:15:08 +0100

    feat(fpga): Add scripts for preparing FPGA flashing

--- elasticai/fpga_testing/__init__.py
+++ elasticai/fpga_testing/__init__.py
@@ -1,2 +1,8 @@
+from .copy_design import (
+    copy_design_arty7_files,
+    copy_design_env5_files,
+    copy_skeleton,
+    copy_vivado_implementation_results
+)
 from .src import *
 from .testing_routine import run_embedded_test
--- elasticai/fpga_testing/src/__init__.py
+++ elasticai/fpga_testing/src/__init__.py
@@ -1,9 +1,3 @@
-from .copy_design import (
-    copy_design_arty7_files,
-    copy_design_env5_files,
-    copy_skeleton,
-    copy_vivado_implementation_results
-)
 from .exp_dut import (
     ConfigurationDUT,
     DeviceUnderTestHandler
