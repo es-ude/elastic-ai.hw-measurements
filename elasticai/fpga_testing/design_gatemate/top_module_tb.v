8787869cd900afbb1caec71974c3809fc4377ed8
David P. Federl <david-peter.federl@uni-due.de>
2026-03-06 21:01:00 +0100

    fix: fpga_testing scripts

--- elasticai/fpga_testing/design_gatemate/top_module_tb.v
+++ elasticai/fpga_testing/design_gatemate/top_module_tb.v
@@ -1,36 +1,40 @@
 `timescale 1 ns / 1 ps
 
+`ifndef DUMPFILE
+`define DUMPFILE "build/sim/TOP_MODULE.vcd"
+`endif
+
 module TOP_MODULE_TB;
-	reg clk;
-	reg rst;
-	wire led;
+  reg  clk;
+  reg  rst;
+  wire led;
 
-    // Clock generation
-	always clk = #100 ~clk;
+  // Clock generation
+  always #100 clk <= ~clk;
 
-	// DUT including
-	TOP_MODULE DUT (
-		.CLK_10MHz(clk),
-		.RSTN(rst),
-		.BTTN(1'd0),
-		.LED(led)
-	);
+  // DUT including
+  TOP_MODULE DUT (
+      .CLK_10MHz(clk),
+      .RSTN(rst),
+      .BTTN(1'd0),
+      .LED(led)
+  );
 
-    // Test procotol
-	initial begin
-        `ifdef CCSDF
-		    $sdf_annotate("blink_00.sdf", DUT);
-        `endif
-		$dumpfile("build/sim/top_module.vcd");
-		$dumpvars(0, TOP_MODULE_TB);
+  // Test procotol
+  initial begin
+`ifdef CCSDF
+    $sdf_annotate("blink_00.sdf", DUT);
+`endif
+    $dumpfile(`DUMPFILE);
+    $dumpvars(0, TOP_MODULE_TB);
 
-		clk = 0;
-		rst = 0;
+    clk = 0;
+    rst = 0;
 
-		#200;
-		rst = 1;
-		#500;
+    #200;
+    rst = 1;
+    #500;
 
-		$stop;
-	end
+    $stop;
+  end
 endmodule
