6fbb6152b1957549400ee94433cdfc734e95c4b8
Andreas Erbslöh <andreas.erbsloeh@uni-due.de>
2026-03-06 12:49:56 +0100

    feat(fpga): Add init stuff

--- elasticai/fpga_testing/__init__.py
+++ elasticai/fpga_testing/__init__.py
@@ -1,8 +1,10 @@
 from .copy_design import (
     copy_design_arty7_files,
     copy_design_env5_files,
+    copy_design_gatemate_files,
     copy_skeleton,
     copy_vivado_implementation_results
 )
+from .scripts_build import *
 from .src import *
 from .testing_routine import run_embedded_test
