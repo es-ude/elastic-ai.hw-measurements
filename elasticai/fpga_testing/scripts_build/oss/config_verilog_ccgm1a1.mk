c472f09c54324f433f67b781c254e403d7521f3a
David P. Federl <david-peter.federl@uni-due.de>
2026-03-06 21:01:00 +0100

    fix: fpga_testing scripts

--- elasticai/fpga_testing/scripts_build/oss/config_verilog_ccgm1a1.mk
+++ elasticai/fpga_testing/scripts_build/oss/config_verilog_ccgm1a1.mk
@@ -1,16 +1,21 @@
 VLOG_SRC = $(shell find $(SRC_ROOT)/ -type f \( -iname "*.v" -o -iname "*.sv" \) ! -iname "*_tb.v")
+CELLS_SYM := oss-cad-suite/share/yosys/gatemate/cells_sim.v
+CPE_LIB := oss-cad-suite/share/yosys/gatemate/cells_bb.v
 
 # synthesize Verilog 
-synth_vlog $(BUILD_OUT)/synth/$(TOP).json $(BUILD_OUT)/synth/$(TOP).v:
+.PHONY: synthesize
+$(BUILD_OUT)/synth/$(TOP).json $(BUILD_OUT)/synth/$(TOP).v:
 	mkdir -p $(BUILD_OUT)/synth
 	yosys \
 		-qql $(BUILD_OUT)/synth/$(TOP)_synth.log \
 		-p 'read_verilog -sv $(VLOG_SRC); hierarchy -top $(TOP); synth_gatemate -top $(TOP) -luttree -nomx8; tee -o $(BUILD_OUT)/$(TOP)_util_sync.rpt stat -width; write_json $(BUILD_OUT)/synth/$(TOP).json; write_verilog $(BUILD_OUT)/synth/$(TOP).v'
+
 # generate bitstream for FPGA
-$(BUILD_OUT)/$(TOP).bin:
+.PHONY: bitstream
+$(BUILD_OUT)/$(TOP).bit: $(BUILD_OUT)/synth/$(TOP).json
 	mkdir -p $(BUILD_OUT)/impl
 	nextpnr-himbaechel \
-	    --top $(TOP) \
+	  --top $(TOP) \
 		--device $(DEVICE) \
 		--json $(BUILD_OUT)/synth/$(TOP).json \
 		-o ccf=$(SRC_ROOT)/io.ccf \
@@ -18,5 +23,13 @@ $(BUILD_OUT)/$(TOP).bin:
 		--router=router2 \
 		--timing-allow-fail \
 		--freq $(FREQ) \
-	    2> $(BUILD_OUT)/$(TOP)_util_impl.rpt
-	gmpack $(BUILD_OUT)/impl/$(TOP).txt $(BUILD_OUT)/$(TOP).bit
+	  2> $(BUILD_OUT)/$(TOP)_util_impl.rpt
+	gmpack $(BUILD_OUT)/impl/$(TOP).txt $@
+
+# generate simualtion for gtkwave 
+.PHONY: simulation
+$(BUILD_OUT)/sim/$(TOP).vcd: $(BUILD_OUT)/synth/$(TOP).json
+	rm -rf $(BUILD_OUT)/sim/*
+	mkdir -p $(BUILD_OUT)/sim
+	iverilog -D'DUMPFILE="$(BUILD_OUT)/sim/$(TOP).vcd"' -o $(BUILD_OUT)/sim/$(TOP).vvp $(PRJ_ROOT)/$(SRC_DIR)/$(TOP).v $(SRC_ROOT)/$(TOP)_tb.v $(CELLS_SYM) $(CPE_LIB)
+	vvp -n $(BUILD_OUT)/sim/$(TOP).vvp
