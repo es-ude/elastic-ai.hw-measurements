27a9222e24c9e8cfb525c81ea74c1c94dc3db0a4
David P. Federl <david-peter.federl@uni-due.de>
2026-03-06 21:01:00 +0100

    fix: fpga_testing scripts

--- elasticai/fpga_testing/scripts_build/oss/config.mk
+++ elasticai/fpga_testing/scripts_build/oss/config.mk
@@ -2,34 +2,17 @@ PRJ_ROOT := $(abspath $(dir $(lastword $(MAKEFILE_LIST)))/../..)
 SRC_ROOT := $(PRJ_ROOT)/$(SRC_DIR)
 TMP_ROOT := $(PRJ_ROOT)/scripts_build/oss
 BUILD_OUT := build
-CELLS_SYM := oss-cad-suite/share/yosys/gatemate/cells_sim.v
-CPE_LIB := $(PRJ_ROOT)/scripts_build/gatemate/cpelib.v
 
 .PHONY: all
-all: clean synthesize bitstream
-
-.PHONY: synthesize
-synthesize: $(BUILD_OUT)/synth/$(TOP).json
-
-.PHONY: bitstream
-bitstream: $(BUILD_OUT)/$(TOP).bin
-
-.PHONY: simulation
-simulation:
-	rm -rf $(BUILD_OUT)/sim/*
-	mkdir -p $(BUILD_OUT)/sim
-	iverilog -o $(BUILD_OUT)/sim/$(TOP).vvp $(PRJ_ROOT)/$(SRC_DIR)/$(TOP).v $(SRC_ROOT)/$(TOP)_tb.v $(CELLS_SYM) $(CPE_LIB)
-	vvp -N $(BUILD_OUT)/sim/$(TOP).vvp
+all: flash
 
 .PHONY: wave
-wave:
-	$(BUILD_OUT)/sim/$(TOP).vcd
+wave: $(BUILD_OUT)/sim/$(TOP).vcd
 	gtkwave $(BUILD_OUT)/sim/$(TOP).vcd
 
 .PHONY: flash
-flash:
-	$(BUILD_OUT)/$(TOP).bit
-	openFPGALoader --cable dirtyJtag --reset --bitstream $(BUILD_OUT)/$(TOP).bin
+flash: $(BUILD_OUT)/$(TOP).bit
+	openFPGALoader --cable dirtyJtag --reset --bitstream $(BUILD_OUT)/$(TOP).bit
 
 .PHONY: clean
 clean:
--- elasticai/fpga_testing/scripts_build/oss/config_verilog_up5k.mk
+++ elasticai/fpga_testing/scripts_build/oss/config_verilog_up5k.mk
@@ -1,13 +1,17 @@
 VLOG_SRC = $(shell find $(SRC_ROOT)/ -type f \( -iname "*.v" -o -iname "*.sv" \) ! -iname "*_tb.v")
+CELLS_SYM := oss-cad-suite/share/yosys/ice40/cell_sim.v
 
 # synthesize Verilog 
-$(BUILD_OUT)/synth/$(TOP).json:
+.PHONY: synthesize
+synth_vlog $(BUILD_OUT)/synth/$(TOP).json:
 	mkdir -p $(BUILD_OUT)/synth
 	yosys \
 	    -qql $(BUILD_OUT)/synth/$(TOP).log \
 	    -p 'read_verilog -sv $(VLOG_SRC); hierarchy -top $(TOP); synth_ice40 -top $(TOP); tee -o $(BUILD_OUT)/$(TOP)_util_sync.rpt stat -width; write_json $@; write_verilog $(BUILD_OUT)/synth/$(TOP).v;'
+
 # implementation
-$(BUILD_OUT)/$(TOP).bin:
+.PHONY: bitstream
+$(BUILD_OUT)/$(TOP).bit: $(BUILD_OUT)/synth/$(TOP).json
 	mkdir -p $(PRJ_ROOT)/$(BUILD_OUT)/impl
 	nextpnr-ice40 \
 		--up5k \
@@ -26,3 +30,11 @@ $(BUILD_OUT)/$(TOP).bin:
 		-r $(BUILD_OUT)/$(TOP)_timing.rpt \
 		$(BUILD_OUT)/impl/$(TOP).asc
 	icepack $(BUILD_OUT)/impl/$(TOP).asc $@
+
+# generate simualtion for gtkwave 
+.PHONY: simulation
+$(BUILD_OUT)/sim/$(TOP).vcd: $(BUILD_OUT)/synth/$(TOP).json
+	rm -rf $(BUILD_OUT)/sim/*
+	mkdir -p $(BUILD_OUT)/sim
+	iverilog -D'DUMPFILE="$(BUILD_OUT)/sim/$(TOP).vcd"' -o $(BUILD_OUT)/sim/$(TOP).vvp $(PRJ_ROOT)/$(SRC_DIR)/$(TOP).v $(SRC_ROOT)/$(TOP)_tb.v $(CELLS_SYM)
+	vvp -n $(BUILD_OUT)/sim/$(TOP).vvp
