15e6f20fda94c0ec052d72e85324c307a80139b5
Andreas Erbslöh <andreas.erbsloeh@uni-due.de>
2026-03-08 09:21:12 +0100

    fix(env5): Change RC and build scripts

--- example/env5_build.py
+++ example/env5_build.py
@@ -2,17 +2,11 @@ from pathlib import Path
 
 from elasticai.fpga_testing import get_path_to_project
 from elasticai.fpga_testing.scripts_build import (
-    translate_bit_to_bin,
-    read_bitstream_file_amd,
-    write_bitstream_into_cheader
+    translate_bit_to_bin
 )
 
 
 if __name__ == "__main__":
-    data = read_bitstream_file_amd(
-        Path(get_path_to_project("fpga_design")) / "design_project.runs" / "impl_1" / "TOP_MODULE.bit", False)
-    write_bitstream_into_cheader(Path(get_path_to_project()) / "TOP_MODULE.bit", data)
-
     translate_bit_to_bin(
         path_to_bitstream_folder=Path(get_path_to_project("fpga_design")) / "design_project.runs" / "impl_1",
         path_to_source=Path(get_path_to_project())
--- example/env5_testing.py
+++ example/env5_testing.py
@@ -14,27 +14,27 @@ def my_custom_command(obj, data):
 
 @rc.main.command
 @click.pass_obj
-def enable_reset(obj):
+def reset_enable(obj):
   rc_handle = rc.RemoteControl(obj)
   rc_handle.send_command(251, "",0)
 
 
 @rc.main.command
 @click.pass_obj
-def disable_reset(obj):
+def reset_disable(obj):
   rc_handle = rc.RemoteControl(obj)
   rc_handle.send_command(252, "", 0)
 
 
 @rc.main.command
 @click.pass_obj
-def do_reset(obj):
+def reset_do(obj):
   rc_handle = rc.RemoteControl(obj)
   for _ in range(10):
       rc_handle.send_command(251, "", 0)
-      sleep(0.01)
+      sleep(0.1)
       rc_handle.send_command(252, "", 0)
-      sleep(0.01)
+      sleep(0.1)
 
 
 if __name__ == '__main__':
