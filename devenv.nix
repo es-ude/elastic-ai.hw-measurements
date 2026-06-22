715bb6caaf9d00f6b067d92496bc8dcbf6a85aec
Andreas Erbslöh <andreas.erbsloeh@uni-due.de>
2026-06-22 22:07:34 +0200

    fix(dev): New github workflow and devenv cmds

--- devenv.nix
+++ devenv.nix
@@ -1,19 +1,47 @@
-{ pkgs, lib, config, inputs, ... }:
-
-
-let
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}: let
   pkgs-rosetta = pkgs.pkgsx86_64Darwin;
-  pkgs-unstable = import inputs.nixpkgs-unstable { system = pkgs.stdenv.system; };
+  pkgs-unstable = import inputs.nixpkgs-unstable {system = pkgs.stdenv.system;};
 in {
   packages = [
     pkgs.git
+    pkgs.tombi
+    pkgs.ruff
+    pkgs.alejandra
     pkgs.wget
     pkgs.gzip
+    pkgs.zlib # needed as dependency cocotb/ghdl under circumstances
     pkgs.gnutar
     pkgs.picotool
   ];
+  cachix.enable = false;
+  languages = {
+    c = {
+      enable = false;
+    };
+    nix = {
+      enable = true;
+    };
+    python = {
+      enable = true;
+      version = "3.13";
+      uv = {
+        enable = true;
+        package = pkgs-unstable.uv;
+      };
+    };
+  };
 
-  scripts = {
+  scripts = let
+    uv_run = "${pkgs-unstable.uv}/bin/uv run --active";
+    alej_run = "${pkgs.alejandra}/bin/alejandra";
+    tombi_run = "${pkgs.tombi}/bin/tombi";
+  in {
     flash_dirty_jtag = {
       exec = ''
         if [[ -e $DEVENV_ROOT/external/dirtyJtag.uf2 ]]; then
@@ -30,7 +58,7 @@ in {
       '';
       package = pkgs.bash;
     };
-    get_latest_oss_cad_suite_release= {
+    get_latest_oss_cad_suite_release = {
       exec = ''
         echo $(wget -qO- "https://api.github.com/repos/YosysHQ/oss-cad-suite-build/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
       '';
@@ -82,22 +110,151 @@ in {
       '';
       package = pkgs.bash;
     };
+    source_oss_cad = {
+      exec = ''
+        if [[ -e $DEVENV_ROOT/oss-cad-suite/environment ]]; then
+          echo "Source oss-cad-suite from local project"
+          cd $DEVENV_ROOT && source /oss-cad-suite/environment
+        elif [[ -e $HOME/oss-cad-suite/environment ]]; then
+          echo "Source oss-cad-suite from home directory"
+          cd $HOME && source oss-cad-suite/environment
+        else
+          echo "oss-cad-suite not found"
+        fi
+        cd $DEVENV_ROOT
+      '';
+    };
+    fix_linting = {
+      exec = ''
+        ${uv_run} ruff format
+        ${uv_run} ruff check --fix
+        ${alej_run} --exclude ./.devenv --exclude ./.devenv.flake.nix .
+        ${tombi_run} format
+      '';
+    };
+    run_tests_all = {
+      exec = ''
+        devenv tasks run test:changes
+      '';
+    };
+    run_tests_local = {
+      exec = ''
+        devenv tasks run check:local
+      '';
+    };
+  };
+
+  tasks = let
+    uv_run = "${pkgs-unstable.uv}/bin/uv run --active";
+    uv_build = "${pkgs-unstable.uv}/bin/uv build";
+  in {
+    "project:sync" = {
+      exec = ''
+        ${uv_run} sync
+      '';
+    };
+    "package:build" = {
+      exec = ''
+        ${uv_build}
+      '';
+    };
+    "docs:check" = {
+      exec = ''
+        export LC_ALL=C  # necessary to run in github action
+        ${uv_run} sphinx-build -b singlehtml docs build/docs
+      '';
+      after = ["docs:clean"];
+    };
+    "docs:build" = {
+      exec = ''
+        export LC_ALL=C  # necessary to run in github action
+        ${uv_run} sphinx-build -j auto -b html docs build/docs
+        touch build/docs/.nojekyll  # prevent github from trying to build the docs
+      '';
+      after = ["docs:clean"];
+    };
+    "docs:clean" = {
+      exec = ''
+        rm -rf build/docs/*
+      '';
+      before = ["docs:build"];
+    };
+    "test:init" = {
+      exec = ''
+        rm -rf .testmondata*
+        ${uv_run} pytest --testmon -m 'not (simulation or slow or plot)' --reruns 3
+      '';
+    };
+    "test:changes" = {
+      exec = ''
+        ${uv_run} pytest --testmon --reruns 3
+      '';
+    };
+    "test:fast" = {
+      exec = ''
+        ${uv_run} pytest -m 'not (simulation or hardware)' --reruns 3
+      '';
+    };
+    "test:hardware" = {
+      exec = ''
+        ${uv_run} pytest -m 'hardware' --reruns 1
+      '';
+    };
+    "test:hardware" = {
+      exec = ''
+        ${uv_run} pytest -m 'simulation' --reruns 1
+      '';
+    };
+    "test:all" = {
+      exec = ''
+        ${uv_run} pytest --reruns 3
+      '';
+    };
+    "test:coverage" = {
+      exec = ''
+        ${uv_run} coverage run -m pytest 'not hardware' --reruns 3
+      '';
+    };
+    "check:coverage-report" = {
+      exec = ''
+        ${uv_run} coverage report -m
+        ${uv_run} coverage xml
+        ${uv_run} coverage html
+      '';
+      after = ["test:coverage"];
+    };
+    "check:toml-lint" = {
+      exec = ''
+        ${uv_run} tombi check .
+      '';
+    };
+    "check:python-lint" = {
+      exec = ''
+        ${uv_run} ruff check .
+      '';
+    };
+    "check:python-types" = {
+      exec = ''
+        ${uv_run} ty check .
+      '';
+    };
+    "check:dependencies" = {
+      exec = ''
+        ${uv_run} pip-audit
+      '';
+    };
+    "check:local" = {
+      after = [
+        "test:fast"
+        "check:python-lint"
+        "check:python-types"
+        "check:toml-lint"
+      ];
+    };
   };
 
   enterShell = ''
-    echo
-    echo "Welcome Back!"
-    echo
     check_oss_cad_available
-    if [[ -e $DEVENV_ROOT/oss-cad-suite/environment ]]; then
-      echo "Source oss-cad-suite from local project"
-      cd $DEVENV_ROOT && source /oss-cad-suite/environment
-    elif [[ -e $HOME/oss-cad-suite/environment ]]; then
-      echo "Source oss-cad-suite from home directory"
-      cd $HOME && source oss-cad-suite/environment
-    else
-      echo "oss-cad-suite not found"
-    fi
-    cd $DEVENV_ROOT
+    source_oss_cad
   '';
 }
