9194608a63d8d5d10231c2dba74c2112711ab60d
Andreas Erbslöh <andreas.erbsloeh@uni-due.de>
2026-07-01 13:00:50 +0200

    feat(fpga.report): Add report extraction for vivado code

---
+++ elasticai/fpga_testing/reports/__init__.py
@@ -0,0 +1,21 @@
+from .vivado import (
+    VivadoHierarchyPowerEntry as VivadoHierarchyPowerEntry,
+)
+from .vivado import (
+    VivadoMetrics as VivadoMetrics,
+)
+from .vivado import (
+    VivadoPowerReport as VivadoPowerReport,
+)
+from .vivado import (
+    VivadoPowerSummary as VivadoPowerSummary,
+)
+from .vivado import (
+    VivadoTimingSummary as VivadoTimingSummary,
+)
+from .vivado import (
+    VivadoUtilizationSummary as VivadoUtilizationSummary,
+)
+from .vivado import (
+    copy_vivado_implementation_results as copy_vivado_implementation_results,
+)
